5632b0ac31c31f95f6ef8e96719b79f8dda00fcc
Saurabh Naik <naiksaurabhd@gmail.com>
2024-10-07 20:07:34 +0530

    added gitignore and some changes to code

--- langchain code.py
+++ langchain code.py
@@ -1,5 +1,9 @@
+import os
+
+from dotenv import load_dotenv
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
@@ -39,16 +43,26 @@ rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
 
 
 
-model_id = "gpt2"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10)
-hf = HuggingFacePipeline(pipeline=pipe)
+# model_id = "gpt2"
+# tokenizer = AutoTokenizer.from_pretrained(model_id)
+# model = AutoModelForCausalLM.from_pretrained(model_id)
+# pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10)
+# hf = HuggingFacePipeline(pipeline=pipe)
+load_dotenv()
+data_directory = os.path.join(os.path.dirname(__file__), "data")
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vector_store = Chroma(embedding_function=embedding_model, persist_directory=data_directory)
+hf_hub_llm = HuggingFaceHub(
+     repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+    # repo_id="mistralai/Mistral-7B-Instruct-v0.3",
+    model_kwargs={"temperature": 1, "max_new_tokens":1024},
+)
 
 qa_chain = (
     {"context": retriever | format_docs, "question": RunnablePassthrough()}
     | rag_prompt
-    | hf
+    | hf_hub_llm
     | StrOutputParser()
 )
 
